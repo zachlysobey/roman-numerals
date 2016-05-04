d86bd091ec03358cc7f8e1bf16202bc295cd361b
Zach Lysobey <Zach.Lysobey@adp.com>
2016-05-03 22:16:09 -0400

    generalize

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 module.exports = romanNumeral
 
+const symbols = [
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+]
+
 function romanNumeral (n) {
   if (n === 0) {
     return ''
   }
-  if (n < 5) {
-    return 'I' + romanNumeral(n - 1)
-  }
-  if (n < 10) {
-    return 'V' + romanNumeral(n - 5)
-  }
-  if (n < 50) {
-    return 'X' + romanNumeral(n - 10)
-  }
-  if (n < 100) {
-    return 'L' + romanNumeral(n - 50)
-  }
-  if (n < 500) {
-    return 'C' + romanNumeral(n - 100)
-  }
-  if (n < 1000) {
-    return 'D' + romanNumeral(n - 500)
+  for (let i = 0; i < symbols.length; i++) {
+    const [sym, val] = symbols[i]
+    const nextVal = symbols[i + 1] ? symbols[i + 1][1] : Infinity
+    if (n < nextVal) {
+      return sym + romanNumeral(n - val)
+    }
   }
 }
